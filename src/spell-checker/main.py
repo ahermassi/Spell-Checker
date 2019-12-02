749b867a0787dada349d01240cf24abf39e3aec0
Anouer Hermassi <ahermass@stevens.edu>
2019-12-01 21:30:22 -0500

    Main program

---
+++ src/spell-checker/main.py
@@ -0,0 +1,16 @@
+from spell_checker import SpellChecker
+
+if __name__ == '__main__':
+
+    print('\nThis is a simple deterministic spell checker. It corrects lower/upper case and mistyped vowels.')
+    print('At the proompt, enter the word you want to spell check.\n')
+
+    spell_checker = SpellChecker()
+
+    while True:
+        word = input('> ')
+        result = spell_checker.spell_check(word.strip().lower())
+        if result == 'No suggestion':
+            print(result)
+        else:
+            print('Did you mean \'' + result + '\' ?')
